a63a825df4fb54183c2946014badce2366b5e910
HongJinone <52749049+HongJinone@users.noreply.github.com>
2019-09-30 20:52:46 +0900

    Dodge_Multi.cs
    게임매니저

---
+++ Multi/MyGameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameManager : MonoBehaviour {
+    public Transform[] s_Point;
+
+    public GameObject p_Cube;
+
+    // Start is called before the first frame update
+    void Start () {
+        // Instantiate(p_Cube, s_Point.position, s_Point.rotation);
+        int myIndexNumber = System.Array.IndexOf(PhotonNetwork.playerList, PhotonNetwork.player);
+        Vector3 location = s_Point[myIndexNumber].position;
+        location.y = 1f;
+
+        PhotonNetwork.Instantiate("Player1",location, s_Point[myIndexNumber].rotation,0);
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Multi/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : Photon.PunBehaviour {
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+    public Camera myCam;
+    private int count;
+    public float rotSpeed = 3.0f;
+    //변수들을 초기화한다
+    private void Awake () {
+        floorMask = LayerMask.GetMask ("Floor");
+        anim = GetComponent<Animator> ();
+        playerRigidbody = GetComponent<Rigidbody> ();
+        myCam = GetComponentInChildren<Camera> ();
+        if (!photonView.isMine) {
+            enabled = false;
+            myCam.gameObject.SetActive (false); //이게 없어서 카메라가 뒤바뀌었던 것
+        }
+    }
+
+    void FixedUpdate () {
+        float h = Input.GetAxisRaw ("Horizontal");
+        float v = Input.GetAxisRaw ("Vertical");
+
+        rotCtrl ();
+        Move (h, v);
+        //Turnning();
+
+    }
+
+    void Move (float h, float v) {
+        movement.Set (h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition (transform.position + movement);
+    }
+    void rotCtrl () {
+        float rotX = Input.GetAxis ("Mouse Y") * rotSpeed;
+        float rotY = Input.GetAxis ("Mouse X") * rotSpeed;
+        myCam.transform.localRotation *= Quaternion.Euler (-rotX, 0, 0);
+        this.transform.localRotation *= Quaternion.Euler (0, rotY, 0);
+
+    }
+    // void Turnning()
+    // {
+    //     Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    //     RaycastHit floorHit;
+
+    //     if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+    //     {
+    //         Vector3 playerToMouse = floorHit.point - transform.position;
+    //         playerToMouse.y = 0f;
+
+    //         Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+    //         playerRigidbody.MoveRotation(newRotation);
+    //     }
+    // }
+
+    //////////////////
+}
